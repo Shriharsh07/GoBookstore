6e7b1d92e7ef928dc65d1b97edec57930ba4c65a
Shriharsh07 <shriharshdpattar@gmail.com>
2024-05-09 11:11:29 +0530

    Model Comments

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -7,6 +7,7 @@ import (
 
 var db *gorm.DB
 
+// Creating Book Model
 type Book struct {
 	gorm.Model
 	Name        string `gorm:"" json:"name"`
@@ -14,30 +15,35 @@ type Book struct {
 	Publication string `json:"publication"`
 }
 
+// The init func help creates the book table
 func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
 
+// Creating book
 func (b *Book) CreateBook() *Book {
 	db.NewRecord(b)
 	db.Create(&b)
 	return b
 }
 
+// Retrieving all books
 func GetAllBooks() []Book {
 	var Books []Book
 	db.Find(&Books)
 	return Books
 }
 
+// Getting Book by Id
 func GetBookById(Id int64) (*Book, *gorm.DB) {
 	var getBook Book
 	db := db.Where("ID=?", Id).Find(&getBook)
 	return &getBook, db
 }
 
+// Deleting Book
 func DeleteBook(ID int64) Book {
 	var book Book
 	db.Where("ID=?", ID).Delete(book)
